724b695bf9060f746b3e1331b75642c736a6568d
linhy1122 <1768013886@qq.com>
2026-05-09 11:00:00 +0800

    first commit

---
+++ src/main/java/org/example/aquabackend/AquaBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.aquabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aquabackend/AquaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aquabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
